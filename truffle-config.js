75e650e10a90fb5f2447d164018dcbbd87f3c298
Bernardo Vieira <bernardo5@live.com.pt>
2019-08-01 10:05:17 +0100

    update openzeppelin repos + update  dependencies

--- truffle-config.js
+++ truffle-config.js
@@ -75,14 +75,14 @@ module.exports = {
 
     // Set default mocha options here, use special reporters etc.
     mocha: {
-        // timeout: 100000
+        timeout: 100000,
         // reporter: 'eth-gas-reporter',
     },
 
     // Configure your compilers
     compilers: {
         solc: {
-            version: '0.5.6', // Fetch exact version from solc-bin (default: truffle's version)
+            version: '0.5.10', // Fetch exact version from solc-bin (default: truffle's version)
             settings: { // See the solidity docs for advice about optimization and evmVersion
                 optimizer: {
                     enabled: false,
